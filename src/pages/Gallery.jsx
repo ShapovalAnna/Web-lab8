5b9ee8b48c8c61ef886d8d3e02755869e3b294cf
anna bc2025-3 <Anna.Shapoval@lnu.edu.ua>
2026-05-05 21:07:13 +0300

    Implemented item click to open QuickView modal

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getInventory } from "../services/inventoryApi";
 import InventoryGallery from "../components/gallery/InventoryGallery";
-
 export default function Gallery() {
     const [items, setItems] = useState([]);
     const navigate = useNavigate();
